aa671eba0aa74f690f7a73dbd6ad77f664221628
enesdemir <enesdemr@outlook.com.tr>
2025-01-16 17:26:27 +0300

    UDPATE

--- app/src/main/java/com/enes/harrypotter/MainActivity.kt
+++ app/src/main/java/com/enes/harrypotter/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.enes.harrypotter
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.enes.harrypotter.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
